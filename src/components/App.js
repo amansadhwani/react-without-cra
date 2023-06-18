8a7c5d710ea79b85b0643493e90f8743617e4d97
Aman Sadhwani <aman.sadhwani@amerisourcebergen.com>
2023-06-18 08:43:31 -0300

    added env variables

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
+    <>
     <h1>App File</h1>
+    <h1>I am running on {process.env.APP_ENV}</h1>
+    <h1>My key is  {process.env.APP_KEY}</h1>
+    </>
   )
 }
 export default App;
