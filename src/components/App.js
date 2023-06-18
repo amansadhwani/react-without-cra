212cb990b8a2f9a9ef07d30e88684fcd29b84003
Aman Sadhwani <aman.sadhwani@amerisourcebergen.com>
2023-06-18 10:24:42 -0300

    added env variables

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
     <h1>App File</h1>
-    <h1>I am running on {process.env.APP_ENV}</h1>
+    <h1>I am running on {process.env.APP_ENV} enviornment</h1>
     <h1>My key is  {process.env.APP_KEY}</h1>
     </>
   )
